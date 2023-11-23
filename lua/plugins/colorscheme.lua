95b40e20899ef05e4289393fb834274dcc897f14
jkholick <jauharkuttyk3@gmail.com>
2023-11-23 22:35:47 +0530

    setting up nvim theme and config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,13 @@ return{
 	{
 	  	"folke/tokyonight.nvim",
     		lazy = false,
-    		opts = { style = "moon" },
+    		opts = { style = "night" },
+		config = function()
+	      		-- load the colorscheme here
+      			vim.cmd([[colorscheme tokyonight-night]])
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    		end,
 	}
 
 }
