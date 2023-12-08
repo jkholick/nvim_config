7a34e4e8c05f179ec329998d748f31e979f9bb70
jkholick <jauharkuttyk3@gmail.com>
2023-12-08 23:43:23 +0530

    Cleaning up configs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,8 +22,8 @@ return
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      	{'hrsh7th/nvim-cmp'},
-		{'hrsh7th/cmp-buffer'},
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
 		{'hrsh7th/cmp-path'},
 		{'saadparwaiz1/cmp_luasnip'},
 		{'hrsh7th/cmp-nvim-lsp'},
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return
 {
     {
-        'nvim-treesitter/nvim-treesitter',
-		init = function()
-				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-				ts_update()
-			end,
+    'nvim-treesitter/nvim-treesitter',
+	init = function()
+		local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		ts_update()
+		end,
     },
 
     {
