a7e311ba0eb7950a310fb5c987348136b9c80a74
jkholick <jauharkuttyk3@gmail.com>
2023-12-08 23:46:51 +0530

    Fix annoying indentation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,10 +24,10 @@ return
     dependencies = {
         {'hrsh7th/nvim-cmp'},
         {'hrsh7th/cmp-buffer'},
-		{'hrsh7th/cmp-path'},
-		{'saadparwaiz1/cmp_luasnip'},
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'hrsh7th/cmp-nvim-lua'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
         {'L3MON4D3/LuaSnip'},
     },
     config = function()
