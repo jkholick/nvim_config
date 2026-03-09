138c63bccb7fbca684d3dc875daf60a6a5f33d3a
jkholick <jauharkuttyk3@gmail.com>
2026-03-09 22:52:35 +0530

    fix: accidentally added two themes in lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,7 +13,6 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		options = { theme = "gruvbox" },
 		config = function()
 			require("lualine").setup({
 				options = { theme = "palenight" },
