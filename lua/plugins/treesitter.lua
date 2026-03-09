56b8df40248b0e30789f6103a051c83e82417ec1
jkholick <jauharkuttyk3@gmail.com>
2026-03-09 23:07:06 +0530

    fix : Actually use treesitter use intented method for lualine instead of calling a config function

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
 	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "lua", "c", "cpp", "python", "html", "bash", "hyprlang" },
+		highlight = { enable = true },
+	},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,10 +13,6 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = { theme = "palenight" },
-			})
-		end,
+		opts = { options = { theme = "palenight" } },
 	},
 }
